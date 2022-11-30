62d557618c9461170e61241f00ae69e6920047f8
Pavlo Sirous <pashka4281@gmail.com>
2022-11-29 23:43:56 -0500

    details page worker

--- run.js
+++ run.js
@@ -1,14 +1,30 @@
 const { Worker } = require('worker_threads');
-//const db = require('./db');
 //
+Array.prototype.inGroupsOf = function(groupSize) {
+  var arr, copy, e, group, res, _i, _len;
+  arr = this;/*from   www  .ja  v a2  s  . c  o  m*/
+  res = [];
+  copy = [];
+  while (arr.length > 0) {
+    group = arr.splice(0, groupSize);
+    res.push(group);
+    copy = copy.concat(group);
+  }
+  for (_i = 0, _len = copy.length; _i < _len; _i++) {
+    e = copy[_i];
+    this.push(e);
+  }
+  return res;
+};
 
 const THREADS = 10;
 
-const request = require('request');
-const cheerio = require('cheerio');
 
 const fetchListOfIndexPages = async function fetchListOfIndexPagesFn() {
-  const url = 'https://www.toronto.ca/data/children/dmc/a2z/a2za.html';
+  const request = require('request');
+  const cheerio = require('cheerio');
+  const url     = 'https://www.toronto.ca/data/children/dmc/a2z/a2za.html';
+
   return new Promise((resolve, reject) => {
     request(url, async function(error, response, body) {
       if (error) {
@@ -40,11 +56,28 @@ const refreshListOfDaycares = async function refreshListOfDaycaresFn() {
   await Promise.all(indexPageUrls.map(url => __createWorker('parse_list_page', url)));
 };
 
+const refreshDetailsOfDaycares = async function refreshListOfDaycaresFn() {
+  const db = require('./db');
+  await db.sequelize.authenticate(); // trying connection to db
+  const daycares = await db.models.Daycare.findAll();
+  await db.sequelize.close();
+
+  console.log('Total daycares in the DB:', daycares.length)
+  const groups = daycares.inGroupsOf(THREADS);
+
+  for (let i = 0; i < groups.length; i++) {
+  //for (let i = 0; i < 1; i++) {
+    //console.log('Group', groups[i]);
+    await Promise.all(groups[i].map(daycare => __createWorker('parse_details_page', daycare.url)));
+  }
+
+};
 
 async function init() {
   // create one single thread
   try {
     //await refreshListOfDaycares();
+    await refreshDetailsOfDaycares();
   } catch(err) {
     console.log('failed to create a thread:', err);
   }
--- workers/parse_details_page.js
+++ workers/parse_details_page.js
@@ -12,16 +12,39 @@ request(workerData.pageUrl, async function(error, response, body) {
   await db.sequelize.authenticate(); // trying connection to db
   //console.log('Status code: ' + response.statusCode);
   var $ = cheerio.load(body);
-  var data = [];
   const address = $('#pfrComplexDescr_sml > div:nth-child(1) > header > p').text().replaceAll(/\n|\t|(Ward: .{1,})/g, '').trim();
+ 
+  const capacityInfant     = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(2)').text().trim();
+  const capacityToddler    = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2)').text().trim();
+  const capacityPreschool  = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(2)').text().trim();
+  
+  const isVacancyInfant    = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(3)').text().trim() === 'Yes';
+  const isVacancyToddler   = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(3)').text().trim() === 'Yes';
+  const isVacancyPreschool = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(3)').text().trim() === 'Yes';
+  
+  const ratingInfant       = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(4) > a').text().trim();
+  const ratingToddler      = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(4) > a').text().trim();
+  const ratingPreschool    = $('#pfrComplexDescr_sml > div.pfrListing > div > header > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(4) > a').text().trim();
 
-  //console.log(data.length, JSON.stringify(data, null, 4));
-  await Daycare.update({ address }, {
-    where: {
-      url: workerData.pageUrl
-    }
-  });
+  const fields = {
+    address,
+    capacityInfant,
+    capacityToddler,
+    capacityPreschool,
+    isVacancyInfant,
+    isVacancyToddler,
+    isVacancyPreschool,
+    ratingInfant,
+    ratingToddler,
+    ratingPreschool,
+  };
+  console.log(fields)
+
+  await db.models.Daycare.update(
+    fields,
+    { where: { url: workerData.pageUrl } }
+  );
   await db.sequelize.close();
-  parentPort.postMessage(data);
+  parentPort.postMessage(fields);
 });
 
